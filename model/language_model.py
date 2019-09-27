0db6726da5cf165e0952c1ea2a578576ac5f2305
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-09-27 19:08:19 +0200

    added embeddings and rnn layer to language model

--- model/language_model.py
+++ model/language_model.py
@@ -10,3 +10,17 @@ class LanguageModel:
 
         self.x = tf.placeholder(tf.int32, [None, max_document_length])
         self.batch_size = tf.shape(self.x)[0]
+
+        self.lm_input = tf.concat([tf.ones([self.batch_size, 1], tf.int32) * word_dict["<s>"], self.x], axis=1)
+        self.lm_output = tf.concat([self.x, tf.ones([self.batch_size, 1], tf.int32) * word_dict["</s>"]], axis=1)
+        self.seq_len = tf.reduce_sum(tf.sign(self.lm_input), 1)
+
+        with tf.variable_scope("embedding"):
+            init_embeddings = tf.random.uniform([self.vocabulary_size, self.embedding_size])
+            embeddings = tf.get_variable("embeddings", initializer=init_embeddings)
+            lm_input_emb = tf.nn.embedding_lookup(embeddings, self.lm_input)
+
+        with tf.variable_scope("rnn"):
+            cell = tf.compat.v1.nn.rnn_cell.BasicLSTMCell(self.num_hidden)
+            rnn_outputs, _ = tf.nn.dynamic_rnn()
+            
