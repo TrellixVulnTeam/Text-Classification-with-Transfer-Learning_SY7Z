e7b6e9375ef127347da98d138349843f04af1ecb
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-09-27 20:56:06 +0200

    created build_word_dict function

--- data_utils.py
+++ data_utils.py
@@ -26,3 +26,33 @@ def clean_str(text):
     text = text.strip().lower()
 
     return text
+
+
+def build_word_dict():
+    if not os.path.exists("word_dict.pickle"):
+        train_df = pd.read_csv(TRAIN_PATH, names=["class", "title", "content"])
+        contents = train_df["content"]
+
+        words = list()
+        for content in contets:
+            for word in word_tokenize(clean_str(content)):
+                words.append(word)
+
+        word_counter = collections.Counter(words).most_common()
+        word_dict = dict()
+        word_dict["<pad>"] = 0
+        word_dict["<unk>"] = 1
+        word_dict["<s>"] = 2
+        word_dict["</s>"] = 3
+        for word, count in word_counter:
+            if count > 1:
+                word_dict[word] = len(word_dict)
+        
+        with open("word_dict.pickle", "wb") as f:
+            pickle.dump(word_dict, f)
+    
+    else:
+        with open("word_dict.pickle", "rb") as f:
+            pickle.load(f)
+    
+    return word_dict
