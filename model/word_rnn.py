938fd4b314dc20b747e676870aed6ddf7bc8d9c9
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-09-30 23:14:53 +0200

    added __init__ to WordRNN

--- model/word_rnn.py
+++ model/word_rnn.py
@@ -3,4 +3,13 @@ from tensorflow.contrib import rnn
 
 
 class WordRNN():
-  pass
+  def __init__(self, vocabulary_size, max_document_length, num_class):
+    self.embedding_size = 256
+    self.num_hiddens = 512
+    self.fc_num_hidden = 256
+
+    self.x = tf.placeholder(tf.int32, [None, max_document_length])
+    self.x_len = tf.reduce_sum(tf.sign(self.x), 1)
+    self.y = tf.placeholder(tf.int32, [None])
+    self.keep_prob = tf.placeholder(tf.float32, [])
+
