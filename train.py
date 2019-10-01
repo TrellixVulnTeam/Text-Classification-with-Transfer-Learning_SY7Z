7bc7187edc96642a1c72f6409dcfac8f6ebf58c1
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-10-01 15:17:17 +0200

    created train function and defined training procedure

--- train.py
+++ train.py
@@ -1,6 +1,7 @@
 import tensorflow as tf
 import argparse
 import os
+from model.word_rnn import WordRNN
 from data_utils import build_word_dataset, build_word_dict, batch_iter, download_dbpedia
 
 
@@ -8,3 +9,16 @@ NUM_CLASS = 14
 BATCH_SIZE = 64
 NUM_EPOCHS = 10
 MAX_DOCUMENT_LEN = 100
+
+
+def train(train_x, train_y, test_x, test_y, vocabulary_size, args):
+  with tf.Session() as sess:
+    model = WordRNN(vocabulary_size, MAX_DOCUMENT_LEN, NUM_CLASS)
+
+    global_step = tf.Variable(0, trainable=False)
+    params = tf.trainable_variables()
+    gradients = tf.gradients(model.loss, params)
+    clipped_gradients, _ = tf.clip_by_global_norm(gradients, 5.0)
+    optimizer = tf.train.AdamOptimizer(0.001)
+    train_op = optimizer.apply_gradients(zip(clipped_gradients, params), global_step=global_step)
+    
