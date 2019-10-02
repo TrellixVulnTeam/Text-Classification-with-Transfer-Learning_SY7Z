cff2ae9fa942c1c041fac44466dc6ffff162b413
Piotrekszmel <piotr.szmelczynski@gmail.com>
2019-10-02 16:58:01 +0200

    fixed bug in train.py

--- train.py
+++ train.py
@@ -24,7 +24,7 @@ def train(train_x, train_y, test_x, test_y, vocabulary_size, args):
         train_op = optimizer.apply_gradients(zip(clipped_gradients, params), global_step=global_step)
 
         # Summary
-        loss_summary = tf.summary.scalar("loss", model.loss)
+        #loss_summary = tf.summary.scalar("loss", model.loss)
         summary_op = tf.summary.merge_all()
         summary_writer = tf.summary.FileWriter(args.summary_dir, sess.graph)
 
@@ -36,7 +36,6 @@ def train(train_x, train_y, test_x, test_y, vocabulary_size, args):
             pre_trained_variables = [v for v in tf.global_variables()
                                      if (v.name.startswith("embedding") or v.name.startswith("birnn")) and "Adam" not in v.name]
             saver = tf.train.Saver(pre_trained_variables)
-            ckpt = tf.train.get_checkpoint_state(os.path.join(args.pre_trained, "model"))
             saver.restore(sess, os.path.join(args.pre_trained, "model"))
 
         
@@ -48,10 +47,10 @@ def train(train_x, train_y, test_x, test_y, vocabulary_size, args):
           }
         
         
-        _, step, summaries, loss = sess.run([train_op, global_step, summary_op, model.loss], feed_dict=feed_dict)
-        summary_writer.add_summary(summaries, step)
+          _, step, summaries, loss = sess.run([train_op, global_step, summary_op, model.loss], feed_dict=feed_dict)
+          summary_writer.add_summary(summaries, step)
 
-        if step % 100 == 0:
+          if step % 100 == 0:
                 print("step {0} : loss = {1}".format(step, loss))
         
 
